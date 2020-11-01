26da04076884b9436bd2ef8be7debd31faa25bc9
ylika <kulini7ch@yandex.ru>
2020-11-02 00:22:53 +0300

    innitial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+    public class Main {
+        public static void main(String[] args) {
+            int ticketAmount = 957;
+            int step = 20;
+            int bonusMile = ticketAmount / step;
+
+            System.out.println(bonusMile);
+        }
+    }
