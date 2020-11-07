fee521307ebe13d9980a2a8f5ac808ef012bfd14
ylika <kulini7ch@yandex.ru>
2020-11-07 21:14:45 +0300

    BonusMilesService added

---
+++ src/BonusMilesService.java
@@ -0,0 +1,10 @@
+public class BonusMilesService {
+    public int calculate (int price){
+        int step = 20;
+        int miles = price / step;
+        return miles;
+
+       }
+
+
+}
--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,9 @@
     public class Main {
         public static void main(String[] args) {
-            int ticketAmount = 957;
-            int step = 20;
-            int bonusMile = ticketAmount / step;
+            BonusMilesService service = new BonusMilesService();
+            int price = 10_000;
+            int miles = service.calculate(price);
+            System.out.println(miles);
 
-            System.out.println(bonusMile);
         }
     }
